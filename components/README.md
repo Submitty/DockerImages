d9bcf3360f3a42f22b931c5613af6e429b14c5cb
Matthew Peveler <matt.peveler@gmail.com>
2018-09-17 16:00:49 -0400

    Add Dockerfiles for Python 2.7, 3.5, and 3.6

---
+++ components/README.md
@@ -0,0 +1,6 @@
+Dockerfile Components
+=====================
+
+Each folder holds dockerfiles related to a given component and specific versions of that component.
+Please navigate into each component to see details on how the component files were created or
+are if unsure.
--- publish.sh
+++ publish.sh
@@ -2,16 +2,17 @@
 
 array=( $(git diff --name-only HEAD~1 HEAD) )
 for i in "${array[@]}"; do
-  if [[ ${i} == dockerfiles/* ]] && [[ ${i} == */Dockerfile ]]; then
-    path=$(echo ${i} | sed 's/\(.*\)\/Dockerfile/\1/g')
-    pushd ${path} > /dev/null
-    tag=$(jq -r '.hostname' metadata.json)/$(jq -r '.name' metadata.json):$(jq -r '.tag' metadata.json)
-    echo -e "Building: ${tag}\n"
-    docker build . -t ${tag}
-    docker push ${tag}
-    echo -e "\n==============="
-    echo -e "   DONE!"
-    echo -e "===============\n"
-    popd > /dev/null
-  fi
+    # Check if file exists, that it's under the dockerfiles folder and that the file is named "Dockerfile"
+    if [ -f ${i} ] && [[ ${i} == dockerfiles/* ]] && [[ ${i} == */Dockerfile ]]; then
+        path=$(echo ${i} | sed 's/\(.*\)\/Dockerfile/\1/g')
+        pushd ${path} > /dev/null
+        tag=$(jq -r '.hostname' metadata.json)/$(jq -r '.name' metadata.json):$(jq -r '.tag' metadata.json)
+        echo -e "Building: ${tag}\n"
+        docker build . -t ${tag}
+        docker push ${tag}
+        echo -e "\n==============="
+        echo -e "   DONE!"
+        echo -e "===============\n"
+        popd > /dev/null
+    fi
 done
